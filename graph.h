7bea95acac57929c0c33494cfb89adda6c6b0a0f
abraham0225 <abraham.rios@trojans.dsu.edu>
2026-05-02 12:12:08 -0500

    correct graph.h file

--- graph.h
+++ graph.h
@@ -1,203 +1,57 @@
-#include "graph.h"
+#ifndef GRAPH_H
+#define GRAPH_H
 
-Graph::Graph(int maxV)
+#include <iostream>
+#include <string>
+#include <iomanip>
+//#include sqll.h  // for traversals
+
+using namespace std;
+
+struct edge
 {
-    maxVertices = maxV;
-    numVertices = 0;
-    vertices = nullptr;
+    struct vertex* dest;
+    edge* eNext;
 
-    visited = new bool[maxVertices] {false};
+    int weight;
+};
 
-    adjMatrix = new int*[maxVertices]; // a pointer of rows
-    // each int* stores a row
-    for(int i = 0; i < maxVertices; i++){
-        adjMatrix[i] = new int[maxVertices];
-        for (int j = 0; j < maxVertices; j++)
-            adjMatrix[i][j] = 0;
-    }
-}
-
-Graph::~Graph()
+struct vertex
 {
-    delete[] visited;
+    char data;
+    vertex* vNext;
+    edge* aHead;
+};
 
-    for(int i = 0; i < maxVertices; i++)
-        delete[] adjMatrix[i];
-
-    delete[] adjMatrix;
-
-    vertex* tempVertex;
-    while(vertices)
-    {
-        edge* tempEdge;
-        // free the memory of the neighbor list of that vertex
-        while(vertices->aHead)
-        {
-            tempEdge = vertices->aHead;
-            vertices->aHead = vertices->aHead->eNext;
-            delete tempEdge;
-        }
-        // free that vertex itself
-        tempVertex = vertices;
-        vertices = vertices->vNext;
-        delete tempVertex;
-    }
-}
-
-void Graph::addVertex(char data)
+class Graph
 {
-    if(numVertices >= maxVertices)
-    {
-        cout << "Max number of vertices reached. Can't add!" << endl;
-        return;
-    }
+    private:
+        vertex* vertices; // list of vertices
+        int** adjMatrix;
+        bool* visited;
+        int maxVertices;
+        int numVertices;
 
-    vertex* newVertex = new vertex;
-    newVertex->data = data;
-    newVertex->vNext = vertices; // add at the beg of the vertices list
-    vertices = newVertex; // make it the new head of the vertex list
-    newVertex->aHead = nullptr;
+    public:
+        Graph(int maxV);
+        ~Graph();
 
-    numVertices++;
-}
+        //core operations
+        void addVertex(char data);
+        void addEdge(char src, char dest, int weight);
+        void removeVertex(char data);
+        void removeEdge(char src, char dest);
 
-void Graph::addEdge(char src, char dest, int weight)
-{
-    vertex* srcVertex = vertices;
-    vertex* destVertex = vertices;
+        // traversal algorithms
+        void bfsL(char start);
+        void bfsM(char start);
+        void dfsL(char start);
+        void dfsM(char start);
 
-    // find src and dest vertices
-    while(srcVertex && srcVertex->data != src)
-    {
-        srcVertex = srcVertex->vNext;
-    }
-    while(destVertex && destVertex->data != dest)
-    {
-        destVertex = destVertex->vNext;
-    }
+        // display
+        void displayList();
+        void displayMatrix();
 
-    if(!srcVertex || !destVertex)
-    {
-        cout << "Vertices not found - can't add an edge" << endl;
-        return;
-    }
+};
 
-    // Adj list - undirected graph
-    edge* newEdge = new edge;
-    newEdge->dest = destVertex;
-    newEdge->weight = weight;
-    newEdge->eNext = srcVertex->aHead;
-    srcVertex->aHead = newEdge;
-
-    // Since undirected, add the reverse edge as well
-    newEdge = new edge;
-    newEdge->dest = srcVertex;
-    newEdge->weight = weight;
-    newEdge->eNext = destVertex->aHead;
-    destVertex->aHead = newEdge;
-
-    // Adj Matrix
-    int srcIndex = srcVertex->data - 'A';
-    int destIndex = destVertex->data - 'A';
-    adjMatrix[srcIndex][destIndex] = weight;
-    adjMatrix[destIndex][srcIndex] = weight;
-}
-
-void Graph::removeVertex(char data)
-{
-    // find the vertex
-    vertex* temp = vertices;
-    vertex* prev = nullptr;
-
-    while(temp && temp->data != data)
-    {
-        prev = temp;
-        temp = temp->vNext;
-    }
-    
-    if(!temp)
-    {
-        cout << "Vertex not found - can't remove" << endl;
-        return;
-    }
-
-    // remove that vertex from the adj list of all other vertices
-    for(vertex* v = vertices; v != nullptr; v = v->vNext)
-    {
-        edge* e = v->aHead;
-        edge* prevEdge = nullptr;
-        while(e)
-        {
-            if(e->dest->data == data)
-            {
-                if(prevEdge)
-                    prevEdge->eNext = e->eNext;
-                else
-                    v->aHead = e->eNext;
-                delete e;
-                break;
-            }
-            prevEdge = e;
-            e = e->eNext;
-        }
-    }
-
-    // remove the vertex
-    if(prev)
-        prev->vNext = temp->vNext;
-    else
-        vertices = temp->vNext;
-
-    delete temp;
-
-    // Adj matrix
-    int index = temp->data - 'A';
-    for(int i = 0; i < maxVertices; i++)
-    {
-        adjMatrix[i][index] = 0;
-        adjMatrix[index][i] = 0;
-    }
-
-    numVertices--;
-}
-
-void Graph::displayList()
-{
-    for(vertex* v = vertices; v != nullptr; v = v->vNext)
-    {
-        cout << v->data << " -> ";
-        for(edge* e = v->aHead; e != nullptr; e = e->eNext)
-        {
-            cout << e->dest->data << " (" << e->weight << ") -> ";
-        }
-        cout << "null" << endl;
-    }
-    cout << endl;
-}
-
-void Graph::displayMatrix()
-{
-    cout << "     ";
-    for(int i = 0; i < maxVertices; i++)
-    {
-        cout << setw(3) << char('A' + i) << " "; // column headers
-    }
-    cout << endl;
-    cout << "   ";
-    
-    for(int i = 0; i < maxVertices; i++)
-    {
-        cout << "-----";
-    }
-    cout << endl;
-
-    for(int i = 0; i < maxVertices; i++)
-    {
-        cout << setw(3) << char('A' + i) << " |"; // row header
-        for(int j = 0; j < maxVertices; j++)
-        {
-            cout << setw(3) << adjMatrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+#endif
